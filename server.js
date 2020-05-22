bd94d72f7bcfcad79a7c7c994f90933e7fad4014
Thalles Ian <thallesyam@users.=.github.com>
2020-05-22 17:06:15 -0300

    Publicando alterações no servidor e colocando imagem dinamica consumindo API do unsplash

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+/* Importando express, nunjucks e importando servidor */
 const express = require('express')
 const nunjucks = require('nunjucks')
-const routes = require('./routes')
 const server = express()
+const routes = require('./routes')
 
+/* Configurando o servidor para utilizar a pasta public */
 server.use(express.static('public'))
+
+/* Chamando a variavel que armazena a pasta de rotas */ 
 server.use(routes)
+
+/* Configurando a view engine nunjucks */
+
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
@@ -12,6 +19,8 @@ nunjucks.configure('views', {
     autoescape: false,
     noCache: true
 })
+
+/* Configurando o servidor para escutar a chamada */
 
 server.listen(5000, function(){
     console.log('server is running')
