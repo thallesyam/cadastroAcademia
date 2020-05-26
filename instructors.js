18b1dd90bae972c914aea89b267c8073b8d54ba0
Thalles Ian <thallesyam@users.=.github.com>
2020-05-25 21:59:57 -0300

    Adicionando botão com a função de deletar e estilizando dinamicamente os instrutores

--- instructors.js
+++ instructors.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const data = require('./data.json')
 const {age, date} = require('./utils')
 
+//index
+exports.index = function(req,res){
+    return res.render('instructors/index', {instructors: data.instructors})
+}
+
 //show
 exports.show = function(req, res){
     const{id} = req.params
@@ -98,3 +103,16 @@ exports.put = function(req, res){
 }
 
 // delete
+exports.delete = function(req, res){
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+    data.instructors = filteredInstructors
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) res.send('Write file error')
+    })
+    return res.redirect('/instructors')
+}
--- routes.js
+++ routes.js
@@ -6,9 +6,7 @@ routes.get('/', function(req, res){
     return res.redirect('/instructors')
 })
 
-routes.get('/instructors', function(req, res){
-    return res.render('instructors/index')
-})
+routes.get('/instructors', instructors.index)
 
 routes.get('/members', function(req, res){
     return res.send('members')
@@ -22,6 +20,8 @@ routes.get('/instructors/:id', instructors.show)
 routes.get('/instructors/:id/edit', instructors.edit)
 
 routes.put('/instructors', instructors.put)
+
+routes.delete('/instructors', instructors.delete)
 
 /* Rotas para receber e validar dados do formulario */
 routes.post('/instructors', instructors.post)
